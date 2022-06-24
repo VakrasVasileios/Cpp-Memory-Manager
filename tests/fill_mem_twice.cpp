939f9fbca5e4f60e029978b4a5987f9801ba59b8
V.Vakras <vasilis_98@hotmail.com>
2022-06-25 02:12:12 +0300

    Benchmark tests

--- tests/fill_mem_twice.cpp
+++ tests/fill_mem_twice.cpp
@@ -3,7 +3,7 @@
 
 int
 main(int argc, char const* argv[]) {
-  int loops = CHUNK_SIZE / sizeof(uint64_t);
+  int loops = (CHUNK_SIZE / sizeof(uint64_t)) * 2;
 
   for (int i = 0; i < loops; i++)
     auto tmp = memman::make_pointer<uint64_t>(i);
--- tests/fill_mem_twice_64B_data.cpp
+++ tests/fill_mem_twice_64B_data.cpp
@@ -1,12 +1,16 @@
 #include "mem_man_bench.hpp"
 #include <stdint.h>
 
+struct data {
+  uint64_t arr[8];
+};
+
 int
 main(int argc, char const* argv[]) {
-  int loops = CHUNK_SIZE / sizeof(uint64_t);
+  int loops = (CHUNK_SIZE / sizeof(data)) * 2;
 
   for (int i = 0; i < loops; i++)
-    auto tmp = memman::make_pointer<uint64_t>(i);
+    auto tmp = memman::make_pointer<data>();
 
   CSV_PRINT;
 
